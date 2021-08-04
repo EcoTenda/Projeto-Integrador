84cca4eae138dc5d36228339e48fcecabe18e6dd
leonardoaf2 <leeo.affonso@hotmail.com>
2021-08-04 15:39:39 -0300

    Criação da model Usuario

---
+++ ecotenda/src/main/java/br/com/ecotenda/model/Usuario.java
@@ -0,0 +1,114 @@
+package br.com.ecotenda.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "Campo nome obrigatório")
+	@Size(min = 5, max = 100)
+	private String nome;
+	
+	@NotNull(message = "Campo CPF obrigatório")
+	@Size(min = 10, max = 20)
+	private String cpf;
+	
+	@NotNull(message = "Campo email obrigatório")
+	@Size(min = 10, max = 50) 
+	private String email;
+	
+	@NotNull(message = "Campo senha obrigatório")
+	@Size(min = 6)
+	private String senha;
+	
+	@NotNull(message = "Campo data de nascimento obrigatório")
+	@JsonFormat(pattern="dd-MM-yyyy")
+	private LocalDate dataNascimento;
+
+	@JsonFormat(pattern="dd-MM-yyyy")
+	private LocalDate dataCriacaoConta = LocalDate.now();
+	
+	@NotNull(message = "Campo tipo do usuario obrigatório")
+	private String tipoUsuario;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public LocalDate getDataCriacaoConta() {
+		return dataCriacaoConta;
+	}
+
+	public void setDataCriacaoConta(LocalDate dataCriacaoConta) {
+		this.dataCriacaoConta = dataCriacaoConta;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	
+}

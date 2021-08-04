53a97ec4bad1a29aa2999015e4e0abaa69617c2f
jonataspribeiro <jonataspalumbo1993@gmail.com>
2021-08-04 13:49:56 -0300

    Criação do CRUD para o Produto

--- ecotenda/src/main/java/br/com/ecotenda/model/Categoria.java
+++ ecotenda/src/main/java/br/com/ecotenda/model/Categoria.java
@@ -1,12 +1,18 @@
 package br.com.ecotenda.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categorias")
@@ -19,9 +25,15 @@ public class Categoria {
 	@NotNull(message = "Valor obrigatorio!")
 	@Size(min = 2, max = 30)
 	private String categoriaProdutos;
+
 	
 	@Size(min = 5, max = 200)
 	private String descricao;
+	
+	@OneToMany(mappedBy = "tb_categorias", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tb_categorias")
+	private List<Produto>produto;
+	
 
 	public long getId() {
 		return id;
